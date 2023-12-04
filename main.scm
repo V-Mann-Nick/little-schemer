df33fd872240ebc125ddf5e0ac57e14ec2cbecfe
Nicklas Sedlock <nicklas.sedlock@posteo.net>
2023-12-04 18:45:14 +0100

    2nd chapter

--- main.scm
+++ main.scm
@@ -20,6 +20,35 @@
   )
 )
 
-(print (lat? (quote ())))
-(print (lat? (quote (hello world again))))
-(print (lat? (quote (hello world (quote (again))))))
+; (print (lat? (quote ())))
+; (print (lat? (quote (hello world again))))
+; (print (lat? (quote (hello world (again)))))
+
+; My implementation
+(define member?
+  (lambda (i l)
+    (cond
+      ((null? l) #f)
+      ((eq? i (car l)) #t)
+      (else (member? i (cdr l)))
+    )
+  )
+)
+
+; Book implementation
+; (define member?
+;   (lambda (a lat)
+;     (cond
+;       ((null? lat) #f)
+;       (else 
+;         (or
+;           (eq? (car lat) a)
+;           (member? a (cdr lat))
+;         )
+;       )
+;     )
+;   )
+; )
+
+(print (member? 'hello '(hello)))
+(print (member? 'hello '(hell world)))
